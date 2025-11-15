5d455c0cbb3672a05d0fa61c493579f24bae74c5
JLChnToZ <jeremylam619@gmail.com>
2025-11-16 00:11:47 +0800

    Add version, etc.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 using System;
 using System.Windows.Forms;
+using System.Reflection;
+
+[assembly: AssemblyTitle("VRCChatbox")]
+[assembly: AssemblyProduct("VRCChatbox")]
+[assembly: AssemblyDescription("A simple input field for VRChat chat box with better support of IME.")]
+[assembly: AssemblyCopyright("Copyright 2025 Jeremy Lam aka. Vistanz. Licensed under MIT.")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
 
 namespace ChatboxApp {
     internal static class Program {
